b7163c5059d0f734b933949e982e855177d82fa1
Zipofar <admin@zipofar.ru>
2019-01-06 18:12:44 +0300

    Add state machine to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,25 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine :initial => :new_task do
+    event :to_dev do
+      transition :new_task => :in_development, :in_qa => :in_development, :in_code_review => :in_development
+    end
+    event :to_archived do
+      transition :new_task => :archived, :released => :archived
+    end
+    event :to_qa do
+      transition :in_development => :in_qa
+    end
+    event :to_code_review do
+      transition :in_qa => :in_code_review
+    end
+    event :to_ready_for_release do
+      transition :in_code_review => :ready_for_release
+    end
+    event :to_release do
+      transition :ready_for_release => :released
+    end
+  end
 end
