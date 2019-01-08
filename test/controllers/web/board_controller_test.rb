d6d422f70198c56ed8fc8aed229ba20b8381d1dd
Zipofar <admin@zipofar.ru>
2019-01-08 16:49:24 +0300

    Add test for admin controller

--- test/controllers/web/board_controller_test.rb
+++ test/controllers/web/board_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Web::BoardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    admin = create(:admin)
+    sign_in_as admin
+  end
+
   test "should get show" do
     get board_url
     assert_response :success
