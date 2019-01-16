c00aca02545397d74e42b34059a12e91df5ce4cd
Zipofar <admin@zipofar.ru>
2019-01-16 12:32:28 +0300

    Fix credentionals

---
+++ public/packs/components/Fetch-2e9f8232164110b0a76a.js
@@ -0,0 +1,110 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 102);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 102:
+/*!**************************************************!*\
+  !*** ./app/javascript/packs/components/Fetch.js ***!
+  \**************************************************/
+/*! exports provided: authenticityToken, fetch */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (immutable) */ __webpack_exports__["authenticityToken"] = authenticityToken;
+/* harmony export (immutable) */ __webpack_exports__["fetch"] = fetch;
+throw new Error("Cannot find module \"axios\"");
+
+
+function authenticityToken() {
+  var token = document.querySelector('meta[name="csrf-token"]');
+  return token ? token.content : null;
+}
+
+function headers() {
+  return {
+    Accept: '*/*',
+    'content-Type': 'application/json',
+    'X-CSRF-Token': authenticityToken(),
+    'X-Requested-With': 'XMLHttpRequest'
+  };
+}
+
+function fetch(method, url, body) {
+  var options = {
+    method: method,
+    headers: headers(),
+    data: body,
+    url: url
+  };
+  return __WEBPACK_IMPORTED_MODULE_0_axios___default()(options);
+}
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=Fetch-2e9f8232164110b0a76a.js.map
