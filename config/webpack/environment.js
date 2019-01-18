f3ac4b3334adf8fb63efd28d06a1e5542e0eb0e2
Zipofar <admin@zipofar.ru>
2019-01-18 17:58:53 +0300

    Add js-routes to globals at webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+environment.plugins.prepend(
+  'Provide',
+   new webpack.ProvidePlugin({
+     'window.Routes': 'Routes',
+   })
+)
