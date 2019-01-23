6a95adc995075c9d0727817aadd33e4363a76942
Zipofar <admin@zipofar.ru>
2019-01-23 09:48:12 +0300

    Refactor build_meta

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,7 @@
 class Api::ApplicationController < ApplicationController
   include Concerns::AuthHelper
 
-  def build_meta_tasks(collection)
+  def build_meta(collection)
     {
       count: collection.count,
       total_count: collection.total_count,
@@ -11,13 +11,11 @@ class Api::ApplicationController < ApplicationController
     }
   end
 
+  def build_meta_tasks(collection)
+    build_meta(collection)
+  end
+
   def build_meta_users(collection)
-    {
-      count: collection.count,
-      total_count: collection.total_count,
-      current_page: collection.current_page,
-      total_pages: collection.total_pages,
-      per_page: collection.limit_value
-    }
+    build_meta(collection)
   end
 end
