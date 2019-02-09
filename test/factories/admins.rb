8640dabda3bdbd3e4a4fb9b66ae7ecad0f379e8b
Zipofar <admin@zipofar.ru>
2019-02-09 20:16:27 +0300

    Refactor factories (add sequence)

--- test/factories/admins.rb
+++ test/factories/admins.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     first_name { generate :string }
     last_name { generate :string }
     password { generate :string }
-    email { "admin@mail.ru" }
+    email { generate :email }
     type { "Admin" }
   end
 end
